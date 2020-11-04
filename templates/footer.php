a0a372220903a27508fb03bb9135862e15e83812
Nicolas-Soroka <sorokanicolas@gmail.com>
2020-11-04 09:02:49 -0300

    change on footer site redirect

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer" style="position: absolute; width: 100%;  height: 40px; line-height: 60px; bottom: 0;">
       <div class="container text-center">
-        <span class="text-muted"><a href="http://escuelaiade.com"><h5>Escuelas IADE</h5></a></span>
+        <span class="text-muted"><a href="https://escuelasiade.com"><h5>Escuelas IADE</h5></a></span>
       </div>
     </footer>
   </body>
