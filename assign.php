00007044322f5537b2735119d18ba8cc4ccbd9ae
Nicolas-Soroka <sorokanicolas@gmail.com>
2020-12-13 16:49:43 -0300

    header problem solved on assign.php too

--- assign.php
+++ assign.php
@@ -1,6 +1,17 @@
 <?php
 require "./templates/header.php";
 
+if (isset($_POST['courseAssign'])) {
+  $nombre = $_POST['id_persona']; //id_persona
+  $course = $_POST['course']; //id curso
+  $pago1 = (isset($_POST['pago']) ? $_POST['pago'] : 0);
+  $cond = 6;
+  $db->query("INSERT INTO curso_p (`id_curso`, `id_persona`, `nivel`, `pago` ) 
+                  VALUES ('$course','$nombre', '$cond', '$pago1');");
+  $_SESSION['mensaje'] = "Curso asignado!";
+  $_SESSION['msg_status'] = 1;
+}
+
 if ($_SESSION['mensaje'] != "") {
   if ($_SESSION['msg_status'] == 1) { ?>
     <div class="alert alert-success text-center"> <?php } else { ?> <div class="alert alert-danger text-center"> <?php } ?>
@@ -9,7 +20,7 @@ if ($_SESSION['mensaje'] != "") {
     <?php  }
   $_SESSION['mensaje'] = ""; ?>
 
-    <form class="container mt-2" action="admin.php" method="post">
+    <form class="container mt-2" action="" method="post">
       <div class="row">
         <div class="col-md-4 mb-3">
           <label for="validationDefault01">Alumno</label>
@@ -44,7 +55,7 @@ if ($_SESSION['mensaje'] != "") {
       </div>
       <div class="form-row">
         <div class="col-md-4 mb-3">
-          <button class="btn btn-primary mt-5" name="btnAccion" value="courseAssign" type="submit">Asignar curso</button>
+          <button class="btn btn-primary mt-5" name="courseAssign" type="submit">Asignar curso</button>
         </div>
       </div>
     </div>
