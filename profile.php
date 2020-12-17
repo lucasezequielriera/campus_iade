2a7e81683874681b2712906117d7be9a20892de1
Nicolas-Soroka <sorokanicolas@gmail.com>
2020-12-17 02:27:21 -0300

    profile photo upload ok!

--- profile.php
+++ profile.php
@@ -1,71 +1,79 @@
 <?php
 require "./templates/header.php";
-
 $persona_id = $_SESSION['user']['id'];
+
+if (isset($_POST['set'])) {
+  $file = $_FILES['file'];
+
+  $fileName = $_FILES['file']['name'];
+  $fileTmpName = $_FILES['file']['tmp_name'];
+  $fileSize = $_FILES['file']['size'];
+  $fileError = $_FILES['file']['error'];
+  $fileType = $_FILES['file']['type'];
+
+  $fileExt = explode('.',$fileName);
+  $fileActualExt = strtolower(end($fileExt));
+  $allowed = array('jpg','jpeg','png');
+
+  if (in_array($fileActualExt, $allowed)) {
+      if ($fileError === 0) {
+          if ($fileSize < 500000) {
+              $fileNameNew =  $persona_id . "." .$fileActualExt;
+              $fileDestination = './img/' . $fileNameNew;
+              if (move_uploaded_file($fileTmpName, $fileDestination)) {
+                $db->query("UPDATE `personas` SET `foto`= '$fileDestination' WHERE id = '$persona_id'");
+                $_SESSION['mensaje'] = "Se ha cargado con exito la imagen!";
+                $_SESSION['msg_status'] = 1;
+              }
+              
+          } else {
+            $_SESSION['mensaje'] = "Error, el archivo supera los 500kb";
+            $_SESSION['msg_status'] = 0;
+          }
+      } else {
+        $_SESSION['mensaje'] = "Error al cargar el archivo";
+        $_SESSION['msg_status'] = 0;
+      }
+  } else { 
+    $_SESSION['mensaje'] = "Error, solo archivos JPG, JPEG o PNG son aceptados.";
+    $_SESSION['msg_status'] = 0;
+  }
+}
+
 $db->query("SELECT * FROM personas WHERE id = $persona_id");
 $datos_usuario = $db->fetch();
 
+if ($_SESSION['mensaje'] != "") {
+  if ($_SESSION['msg_status'] == 1) { ?>
+    <div class="alert alert-success text-center"> <?php } else { ?> <div class="alert alert-danger text-center"> <?php } ?>
+      <?php echo $_SESSION['mensaje']; ?>
+      </div>
+    <?php  }
+  $_SESSION['mensaje'] = ""; ?>
 
-if (isset($_FILES["file"])) {
-
-    $directoryName = './cursos/' . $nombre;
-    $examDirectory = $directoryName . '/' . 'exams/';
-    $target_dir = $directoryName . '/';
-    $target_file = $target_dir . basename($_FILES["file"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-    $check = getimagesize($_FILES["file"]["tmp_name"]);
-    if ($check !== false) {
-      $uploadOk = 1;
-    } else {
-      $uploadOk = 0;
-    }
-
-    if ($_FILES["file"]["size"] > 1100000) {  // MAX 1mb
-      $err = "Error, el archivo supera 1 Mb";
-      $uploadOk = 0;
-    }
-
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-      $err = "Error, solo archivos JPG, JPEG o PNG son aceptados.";
-      $uploadOk = 0;
-    }
-
-  if ($uploadOk == 1) {
-    if (move_uploaded_file($_FILES["file"]["tmp_name"],$target_file)) {
-      $db->query("INSERT INTO personas (`imagen`)
-      VALUES ('$target_file') WHERE id = '$'");
-    }
-  }
-}
-?>
-
-<div class="container mt-4">
-  <form action="">
-    <div class="row">
-      <img src="<?= $datos_usuario['foto']; ?>" alt="profile-photo" id="profile-photo" style="max-height: 200px; border: 3px solid black;">
-    </div>
-    <div class="row">
-        <form action="" method="post">
-          <div class="form-group">
-            <label class="btn" for="my-file-selector">
-              <input required type="file" name="file" id="exampleInputFile">
-            </label>      
+    <div class="container mt-4">
+      <form action="" id="form_photo" method="POST" enctype="multipart/form-data">
+        <div class="row">
+          <div class="image-upload">
+            <label for="file-input">
+              <img src="<?= $datos_usuario['foto']; ?>" alt="profile-photo" id="profile-photo" style="max-height: 200px; border: 3px solid black;" />
+            </label>
+            <input type="hidden" value="1" name="set">
+            <input id="file-input" required type="file" name="file" accept="image/png, image/jpeg, image/jpg" style="display: none;" />
           </div>
         </form>
       </div>
-      <!-- DOCUMENTO -->
-    <div hidden class="row mt-4"">
-      <div class="col-md-4 mb-3">
-        <label for="">Documento</label>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroupPrepend2">#</span>
+      <form action="" method="post">
+        <div hidden class="row mt-4"">
+      <div class=" col-md-4 mb-3">
+          <label for="">Documento</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupPrepend2">#</span>
+            </div>
+            <input type="number" disabled class="form-control" name="dni" id="dni_user" value="<?= $datos_usuario['dni']; ?>" aria-describedby="inputGroupPrepend2" required>
           </div>
-          <input type="number" disabled class="form-control" name="dni" id="dni_user" value="<?= $datos_usuario['dni']; ?>" aria-describedby="inputGroupPrepend2" required>
         </div>
-      </div>
     </div>
 
     <div class="row mt-4">
@@ -97,42 +105,40 @@ if (isset($_FILES["file"])) {
     </div>
     <button class="btn btn-success" onclick='update();' type="submit">Guardar cambios</button>
     <a href="index.php" class='btn btn-danger ml-3'>Cancelar</a>
-  </form>
-  <div class="botones"></div>
-</div>
+    </form>
+    </div>
 
-<script>
-  function update() {
-    if (!($('#nombre').val() && $('#apellido').val())) {
-      alert("Nombre y apellido son obligatorios");
-      return;
-    }
-    var info = {
-      "nombre": $("#nombre").val(),
-      "apellido": $("#apellido").val(),
-      "mail": $("#email").val(),
-      "tel": $("#tel").val(),
-      "dni": $("#dni_user").val()
-    };
-
-    if (info) {
-      $.ajax({
-        type: 'POST',
-        url: 'update.php',
-        data: info,
-        success: function(response) {
-          $("#nombre").val();
-          $("#apellido").val();
-          $("#email").val();
-          $("#tel").val();
-          alert("Se modificaron los datos.");
-          location.reload();
+    <script>
+      function update() {
+        if (!($('#nombre').val() && $('#apellido').val())) {
+          alert("Nombre y apellido son obligatorios");
+          return;
         }
-      });
-    }
-  }
-</script>
+        var info = {
+          "nombre": $("#nombre").val(),
+          "apellido": $("#apellido").val(),
+          "mail": $("#email").val(),
+          "tel": $("#tel").val(),
+          "dni": $("#dni_user").val()
+        };
 
-<?php
-require "./templates/footer.php";
-?>
+        if (info) {
+          $.ajax({
+            type: 'POST',
+            url: 'update.php',
+            data: info,
+            success: function(response) {
+              location.reload();
+            }
+          });
+        }
+      }
+
+      document.getElementById("file-input").onchange = function() {
+        document.getElementById("form_photo").submit();
+      };
+    </script>
+
+    <?php
+    require "./templates/footer.php";
+    ?>
--- templates/header.php
+++ templates/header.php
@@ -16,7 +16,6 @@ if (!isset($_SESSION['logged'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IADE Campus</title>
-    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -24,6 +23,7 @@ if (!isset($_SESSION['logged'])) {
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
     <style>
         input[type='number'] {
             -moz-appearance: textfield;
