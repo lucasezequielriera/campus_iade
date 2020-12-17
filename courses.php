1639e8702bf0d026f382b3215df8f08735a54f6a
Nicolas-Soroka <sorokanicolas@gmail.com>
2020-12-16 23:52:52 -0300

    fixed bug when u try to create a new course with an already existing name

--- courses.php
+++ courses.php
@@ -12,56 +12,63 @@ if (isset($_POST['newCourse'])) {
   $uploadOk = 1;
   $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-  // Check por si es un archivo valido (>0)
-  $check = getimagesize($_FILES["file"]["tmp_name"]);
-  if ($check !== false) {
-      $uploadOk = 1;
-  } else {
-      $uploadOk = 0;
-  }
-  //hacer el check por si no es imagen.
-  if (file_exists($target_file)) {
-      $err = "Error, ya existe un archivo con ese nombre";
-      $uploadOk = 0;
+  //check si el curso ya existe
+  $db->query("SELECT `nombre` FROM `curso` WHERE `nombre` = '$nombre' LIMIT 1");
+  if ($db->numRows() == 1) {
+    $err = "Error, ya existe un curso con ese nombre";
+    $uploadOk = 0;
+  } 
+  else {
+    // Check por si es un archivo valido (>0)
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
+    if ($check !== false) {
+        $uploadOk = 1;
+    } else {
+        $uploadOk = 0;
+    }
+    //hacer el check por si no es imagen.
+    if (file_exists($target_file)) {
+        $err = "Error, ya existe un archivo con ese nombre";
+        $uploadOk = 0;
+    }
+
+    if ($_FILES["file"]["size"] > 512000) {  // MAX 500Kb
+        $err = "Error, el archivo supera los 500kb";
+        $uploadOk = 0;
+    }
+
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+        $err = "Error, solo archivos JPG, JPEG o PNG son aceptados.";
+        $uploadOk = 0;
+    }
   }
 
-  if ($_FILES["file"]["size"] > 512000) {  // MAX 500Kb
-      $err = "Error, el archivo supera los 500kb";
-      $uploadOk = 0;
-  }
-
-  if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-      $err = "Error, solo archivos JPG, JPEG o PNG son aceptados.";
-      $uploadOk = 0;
-  }
-
-  if ($uploadOk == 0) {
-      $_SESSION['mensaje'] = $err;
-      $_SESSION['msg_status'] = 0;                              //Si hubo error se redirige a courses.php
-  } else {
-      //creacion del directorio del curso
-      if (!is_dir($directoryName)) {
-          mkdir($directoryName, 0777);
-          for ($i = 0; $i < 6; $i++) {
-              $directoryName = 'cursos/' . $nombre . '/Modulo ' . ($i + 1);
-              mkdir($directoryName, 0777);
-          }
-          $dir_exam = $target_dir . "/exams"."/exam.php";
-          mkdir($dir_exam,0777);
-
-      }
-      //volcado de la informacion a la base de datos //carga de la imagen en directorio del curso
-      if (move_uploaded_file($_FILES["file"]["tmp_name"],$target_file)) {
-          $nombre = $_POST['nombre'];
-          $db->query("INSERT INTO `curso`(`nombre`, `url_doc`, `imagen`, `descripcion`, `exams`, `categoria`)
-                      VALUES ('$nombre','$target_dir', '$target_file', '$descripcion_curso','$examDirectory' , '$categoria')");
-          $_SESSION['mensaje'] = "Se ha creado con exito el curso " . $nombre;
-          $_SESSION['msg_status'] = 1;
-      } else {
-          $_SESSION['mensaje'] = ("Hubo un error al subir el archivo: " . $err);
+      if ($uploadOk == 0) {
+          $_SESSION['mensaje'] = $err;
           $_SESSION['msg_status'] = 0;
+      } else {
+          //creacion del directorio del curso
+          if (!is_dir($directoryName)) {
+              mkdir($directoryName, 0777);
+              for ($i = 0; $i < 6; $i++) {
+                  $directoryName = 'cursos/' . $nombre . '/Modulo ' . ($i + 1);
+                  mkdir($directoryName, 0777);
+              }
+              $dir_exam = $target_dir . "exams";
+              mkdir($dir_exam, 0777);
+          }
+          //volcado de la informacion a la base de datos //carga de la imagen en directorio del curso
+          if (move_uploaded_file($_FILES["file"]["tmp_name"],$target_file)) {
+              $nombre = $_POST['nombre'];
+              $db->query("INSERT INTO `curso`(`nombre`, `url_doc`, `imagen`, `descripcion`, `exams`, `categoria`)
+                          VALUES ('$nombre','$target_dir', '$target_file', '$descripcion_curso','$examDirectory' , '$categoria')");
+              $_SESSION['mensaje'] = "Se ha creado con exito el curso " . $nombre;
+              $_SESSION['msg_status'] = 1;
+          } else {
+              $_SESSION['mensaje'] = ("Hubo un error al subir el archivo: " . $err);
+              $_SESSION['msg_status'] = 0;
+          }
       }
-  }
 }
 
 if ($_SESSION['mensaje'] != "") {
--- upload_.php
+++
@@ -1,6 +0,0 @@
-<?php
-$fichero = $_FILES["file"];
-move_uploaded_file($fichero["tmp_name"], $_POST['dir_upload'] . $fichero["name"]);
-header("Location: content.php");
-exit();
-?>
